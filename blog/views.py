58d5882a36280824073f5f5ca8c3b42f842dd621
LucasDiorio <lucas.diorio@gmail.com>
2023-01-02 13:10:04 -0300

    carga de template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def inicio(request):
+    return render(request, "blog/inicio.html")
+
+def aboutme(request):
+    return render(request, "blog/aboutme.html")
